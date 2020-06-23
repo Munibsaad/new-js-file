b4983fd50d9e4af84b79df267a5c0cd9874d6f2b
abcweb <munibsaad12@gmail.com>
2020-06-23 15:23:11 +0500

    ready js file

---
+++ app.js
@@ -0,0 +1,51 @@
+//  var first= prompt("Enter your first name")
+
+//  var second =  prompt("Enter your second name")
+ 
+//  var merg = document.write(first+second)
+
+
+// var name =prompt("Enter your name")
+// var length = name.length
+// var abx =alert("length of a string ="+length)
+
+
+// var name = "Munib Saad"
+// console.log(name.indexOf("d"))
+
+// var rep = "hello islamabad  i'm comminbg"
+// alert(rep.replace("comminbg","comming back"))
+
+
+// var cap = prompt("enter your name")
+// var caps = (cap.toUpperCase());
+// alert(caps)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
